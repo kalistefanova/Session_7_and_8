7cfa93d371da0d774491743a43396f63597130f8
kalistefanova <kalinaste@gmail.com>
2025-01-28 16:46:33 +0100

    added simple strings examples

---
+++ simple_strings.py
@@ -0,0 +1,13 @@
+#from xml.dom.pulldom import SAX2DOM
+
+s1 = "John" # double quote
+s2 = 'Mary' # single quote
+
+print(s1)
+print(s2)
+
+s1 = "And then he asked:'How are you?'"
+print(s1)
+s2 = 'And then he asked:"How are you?"'
+print(s2)
+s1 = "This is how to have \" and \' inside a string" # you can also escape them
